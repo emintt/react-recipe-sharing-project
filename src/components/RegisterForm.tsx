6f5b95f18ab3d17a74decbef80e11b000d61b63d
liem nguyen <elminguyen@gmail.com>
2024-03-02 02:02:35 +0200

    register

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,9 +1,101 @@
+import { useState } from "react";
+import { useUser } from "../hooks/apiHooks";
+import { useForm } from "../hooks/formHooks";
 
 
 const RegisterForm = () => {
+  const initValues = {username: '', password: '', email: ''};
+
+  const {postUser} = useUser();
+
+  const doRegister = async () => {
+    try {
+      console.log(inputs);
+      if (usernameAvailable && emailAvailable) {
+        await postUser(inputs);
+      }
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  }
+
+  const [usernameAvailable, setUsernameAvailable] = useState<boolean>(true);
+  const [emailAvailable, setEmailAvailable] = useState<boolean>(true);
+
+  const {getUsernameAvailable, getEmailAvailable} = useUser();
+  const handleUsernameBlur = async () => {
+    const result = await getUsernameAvailable(inputs.username);
+    setUsernameAvailable(result.available);
+  }
+
+  const handleEmailBlur = async (
+    event: React.SyntheticEvent<HTMLInputElement>
+    ) => {
+    console.log(event.currentTarget.value);
+    const result = await getEmailAvailable(event.currentTarget.value);
+    setEmailAvailable(result.available);
+  }
+
+  const {handleSubmit, handleInputChange, inputs} = useForm(doRegister, initValues);
+
   return (
     <>
-      <h3>Register page kdsnsncjbdychdsbj</h3>
+      <div className=" flex flex-col items-center gap-5 my-6">
+        <h3 className=" text-3xl text-center">Rekisteröidy</h3>
+        <form onSubmit={handleSubmit} className=" flex flex-col py-6 px-3 border rounded">
+          <div className=" flex justify-between items-center">
+            <label
+              className=" p-3"
+              htmlFor="username">
+                Username
+            </label>
+            <input
+              className=" text-slate-950 w-96 m-3 rounded p-2 border-slate-500"
+              name="username"
+              type="text"
+              id="username"
+              onChange={handleInputChange}
+              onBlur={handleUsernameBlur}
+              autoComplete="username"
+            />
+          </div>
+          <div className=" flex justify-between">
+              <label className=" p-3" htmlFor="password">Password</label>
+              <input
+                className=" text-slate-950 w-96 m-3 rounded p-2 border-slate-500"
+                name="password"
+                type="password"
+                id="password"
+                onChange={handleInputChange}
+                autoComplete="current-password"
+              />
+          </div>
+          <div className=" flex justify-between">
+              <label className=" p-3" htmlFor="passwordConfirm">Confirm Password</label>
+              <input
+                className=" text-slate-950 w-96 m-3 rounded p-2 border-slate-500"
+                name="passwordConfirm"
+                type="password"
+                id="passwordConfirm"
+                onChange={handleInputChange}
+                autoComplete="current-password"
+              />
+          </div>
+          <div className=" flex justify-between items-center">
+            <label className=" p-3" htmlFor="email">Email</label>
+            <input
+              className=" text-slate-950 w-96 m-3 rounded p-2 border-slate-500"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleInputChange}
+              onBlur={handleEmailBlur}
+              autoComplete="email"
+            />
+          </div>
+          <button className="m-3 w-1/3 rounded-md bg-slate-700 p-3 self-center" type="submit">Rekisteröidy</button>
+        </form>
+      </div>
     </>
   );
 }
--- src/hooks/apiHooks.ts
+++ src/hooks/apiHooks.ts
@@ -45,7 +45,36 @@ const useUser = () => {
     );
   }
 
-  return {getUserByToken};
+  const postUser = async (user: Record<string, string>) => {
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+
+    await fetchData<UserResponse>(
+      import.meta.env.VITE_AUTH_API + '/users',
+      options,
+    );
+  };
+
+  const getUsernameAvailable = async (username: string) => {
+    const result = await fetchData<{available: boolean}>(
+      import.meta.env.VITE_AUTH_API + '/users/username/' + username
+    );
+    return result;
+  };
+
+  const getEmailAvailable = async (email: string) => {
+    const result = await fetchData<{available: boolean}>(
+      import.meta.env.VITE_AUTH_API + '/users/email/' + email
+    );
+    return result;
+  };
+
+  return {getUserByToken, postUser, getUsernameAvailable, getEmailAvailable};
 }
 const useAuthentication = () => {
   const postLogin = async (creds: Credentials) => {
